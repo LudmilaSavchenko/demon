bca7ccd8c704ef887d1bf8df1b27ea7b2c7be01e
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2024-05-20 22:38:50 +0300

    ES45: добавление класса репозитория к таблице

---
+++ src/main/java/com/example/demon/repository/CustomersRepository.java
@@ -0,0 +1,9 @@
+package com.example.demon.repository;
+
+import com.example.demon.domain.Customers;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomersRepository extends JpaRepository<Customers, Long> {
+}
