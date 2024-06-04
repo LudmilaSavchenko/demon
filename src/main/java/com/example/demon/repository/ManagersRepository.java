06195e4593cf4532e8911a013ff3c9fe14e5a71d
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2024-06-04 22:11:47 +0300

    ES49: Реализовать метод 2

--- src/main/java/com/example/demon/repository/ManagersRepository.java
+++ src/main/java/com/example/demon/repository/ManagersRepository.java
@@ -1,14 +1,13 @@
 package com.example.demon.repository;
 
-import com.example.demon.domain.Customers;
 import com.example.demon.domain.Managers;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ManagersRepository extends JpaRepository<Managers, Long> {
     List<Managers> findAll();
     List<Managers> findTop1ByFirstNameRu(String fistName);
+    Managers findTop1ByTimeZone(String timeZone);
 }
--- src/main/java/com/example/demon/service/ManagersService.java
+++ src/main/java/com/example/demon/service/ManagersService.java
@@ -1,15 +1,14 @@
 package com.example.demon.service;
 
-import com.example.demon.domain.Customers;
 import com.example.demon.domain.Managers;
 import com.example.demon.repository.ManagersRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 
 @Service
 public class ManagersService {
+    private final String MOSCOW_TIME_ZONE = "GMT+3";
     private final ManagersRepository managersRepository;
 
     public ManagersService(ManagersRepository managersRepository) {
@@ -22,5 +21,19 @@ public class ManagersService {
 
     public List<Managers> findTop1ByFirstNameRu(String fistName) {
         return managersRepository.findTop1ByFirstNameRu(fistName);
+    }
+
+    public Managers findTop1ByTimeZone(String timeZone) {
+        return managersRepository.findTop1ByTimeZone(timeZone);
+    }
+
+    public Managers getManagerByTimeZone(String timeZone){
+        Managers manager = findTop1ByTimeZone(timeZone);
+        if (manager == null ){
+            return findTop1ByTimeZone(MOSCOW_TIME_ZONE);
+        }
+        else{
+            return manager;
+        }
     }
 }
